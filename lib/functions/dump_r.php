7279d230b35c467f195c27c2bfcc6b2b4e671cb9
Paul GOERGLER <pgoergler@gmail.com>
2015-07-23 19:44:55 +0400

    add more functions
    add \dump_simple_var()
    add \is_simple_var()
    add \dump_flat()

--- lib/functions/dump_r.php
+++ lib/functions/dump_r.php
@@ -9,7 +9,7 @@ function dump_key($key)
     return "'$key'";
 }
 
-function dump_r($variable, $padChar = "    ", $level = 0)
+function dump_simple_var($variable, $qualifyAll = true)
 {
     if (is_null($variable))
     {
@@ -30,6 +30,52 @@ function dump_r($variable, $padChar = "    ", $level = 0)
     {
         return "$variable";
     }
+    if ($qualifyAll)
+    {
+        if (is_object($variable))
+        {
+            return "\\" . get_class($variable) . "{...}";
+        }
+        if (is_array($variable))
+        {
+            return "array(...)";
+        }
+
+        return gettype($variable) . "{...}";
+    }
+    return null;
+}
+
+function is_simple_var($variable)
+{
+    if (is_null($variable))
+    {
+        return true;
+    }
+
+    if (is_bool($variable))
+    {
+        return true;
+    }
+
+    if (is_string($variable))
+    {
+        return true;
+    }
+
+    if (is_numeric($variable))
+    {
+        return true;
+    }
+    return false;
+}
+
+function dump_r($variable, $padChar = "    ", $depth = 0, $maxDepth = null)
+{
+    if (is_simple_var($variable))
+    {
+        return \dump_simple_var($variable);
+    }
 
     if (is_array($variable) || $variable instanceof \Iterator || is_object($variable))
     {
@@ -37,12 +83,14 @@ function dump_r($variable, $padChar = "    ", $level = 0)
         {
             $class = "array";
             $openTag = "(";
+            $joinTag = " => ";
             $closeTag = ")";
             $array = $variable;
         } else
         {
             $class = '\\' . get_class($variable);
-            $openTag = " {";
+            $openTag = "{";
+            $joinTag = " = ";
             $closeTag = "}";
 
             if (method_exists($variable, 'toArray'))
@@ -56,14 +104,37 @@ function dump_r($variable, $padChar = "    ", $level = 0)
 
         $lines = "{$class}{$openTag}";
         $hasProperties = false;
+        $first = true;
         foreach ($array as $key => $value)
         {
             $hasProperties = true;
-            $lines .= "\n" . str_repeat($padChar, $level + 1) . \dump_key($key) . " => " . \dump_r($value, $padChar, $level + 1) . ",";
+            $lines .= ($first ? "" : "," ) . "\n" . str_repeat($padChar, $depth + 1) . \dump_key($key) . $joinTag . ( is_null($maxDepth) || $depth < $maxDepth ? \dump_r($value, $padChar, $depth + 1, $maxDepth) : \dump_simple_var($value, true));
+            $first = false;
         }
-        return $lines . ( $hasProperties ? "\n" . str_repeat($padChar, $level) : '') . "$closeTag";
+        return $lines . ( $hasProperties ? "\n" . str_repeat($padChar, $depth) : '') . "$closeTag";
     } else
     {
-        return \dump_r(array_map('trim', explode("\n", print_r($variable, true))), $padChar, $level + 1);
+        return \dump_r(array_map('trim', explode("\n", print_r($variable, true))), $padChar, $depth + 1, $maxDepth);
     }
+}
+
+function dump_flat($variable, $maxDepth, $padChar = "    ")
+{
+    $rows = array_map('trim', explode("\n", \dump_r($variable, $padChar, 0, $maxDepth)));
+    $flat = "";
+
+    $first = true;
+    foreach ($rows as $row)
+    {
+        if (!$first)
+        {
+            if (!in_array($row, ['{', '}', '(', ')', '[', ']']))
+            {
+                $flat .= " ";
+            }
+        }
+        $flat .= $row;
+        $first = false;
+    }
+    return $flat;
 }
